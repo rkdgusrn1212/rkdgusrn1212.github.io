a522767535a3a845c998d0eec155bb57542f07ba
rkdgusrn1212 <khgkjg12@naver.com>
2022-11-30 17:04:55 +0900

    React 앱을 Jekyll 웹 메인에 띄우는 기능 구현
    - Jekyll앱은 _site에 정적 웹 페이지&파일들을 빌드. _site/가 구동될 웹
    서버의 루트이므로 _site/index.html이 메인 페이지. Jekyll은
    _site/idnex.html을 빌드하기위해 _layouts/host.html을 사용. 따라서
    React의 최종 빌드된 build/index.html을 _layout/host.html에
    복제했다.(package.json에 구성)
    - React는 build/index.html에서 build/경로에 포함된 빌드된 정적 파일들을 작동에 필요로한다. 따라서 Jekyll의 빌드 출력 폴더에 해당 정적파일들을 포함시켜야 했고, 그러기위해 Jekyll 프로젝트 루트에 react-static 폴더를 생성, Jekyll의 빌드 폴더에 그대로 복제 _site/react-static에 포함되게 하였다. 또한 index.html와 그에 따른 연쇄적인 파일 부르기에서 서버에 올라간 정적 파일들의 경로(/react-static/...)을 올바르게 참조하게 하기위해 React 앱의 homepage값을 react-static으로 지정하여 빌드된 파일들이 참조하는 절대경로가 /react-static으로 시작하게 하였다.

---
+++ dev-blog-react/src/components/Footer.js
@@ -0,0 +1,66 @@
+import Container from "react-bootstrap/Container";
+import Table from "react-bootstrap/Table";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Badge from "react-bootstrap/Badge";
+
+function Footer() {
+  return (
+    <Container fluid="true" className="bg-dark bg-gradient">
+      <Row className="p-1">
+        <Col xs="12" sm="5" className="p-2">
+          <Row className="p-3">
+            <Col><h1 className="text-light text-center">9Log</h1></Col>
+          </Row>
+          <Row className="p-3">
+            <Col className="d-flex flex-wrap justify-content-evenly">
+              <a className="text-light" href="#">깃헙</a>
+              <a className="text-light" href="#">백준</a>
+              <a className="text-light" href="#">구 블로그</a>
+            </Col>
+          </Row>
+          <Row className="p-3">
+            <Col className="d-flex flex-wrap">
+              <Badge className="m-2">Spring</Badge>
+              <Badge className="m-2">JPA</Badge>
+              <Badge className="m-2">Java</Badge>
+              <Badge className="m-2">Oracle</Badge>
+              <Badge className="m-2">MyBatis</Badge>
+              <Badge className="m-2">Hibernate</Badge>
+              <Badge className="m-2">Bootstrap</Badge>
+              <Badge className="m-2">React</Badge>
+              <Badge className="m-2">MySQL/MariaDB</Badge>
+              <Badge className="m-2">JSP/Servlet</Badge>
+            </Col>
+          </Row>
+        </Col>
+        <Col xs="12" sm="7" className="p-2">
+          <Table borderless className="text-light">
+            <thead>
+              <tr>
+                <th>Posts</th>
+                <th>Projects</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Java</td>
+                <td>오목판</td>
+              </tr>
+              <tr>
+                <td>Algorithm</td>
+                <td>Tasty Way</td>
+              </tr>
+              <tr>
+                <td>Spring</td>
+                <td>두부북쉐어</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
