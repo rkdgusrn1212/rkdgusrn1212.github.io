044529a3f98af86955649bcbe64842b0a05dab7c
rkdgusrn1212 <khgkjg12@naver.com>
2023-03-29 11:23:39 +0900

    기존 slice 제거, 더 이상 안씀

--- dev-blog-react/src/store.ts
+++ dev-blog-react/src/store.ts
@@ -1,11 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import { postInfoByIdxSlice } from './services/postInfoByIdxSlice';
-import { useDispatch } from 'react-redux';
 import postApi from 'services/postApi';
 
 const store = configureStore({
   reducer: {
-    postInfo: postInfoByIdxSlice.reducer,
     postApi: postApi.reducer,
   },
   middleware: (gDM) => gDM().concat(postApi.middleware),
