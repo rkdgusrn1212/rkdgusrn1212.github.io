8b49d6f5e2351f3bdf8088bf86318cc1a4e6cb53
rkdgusrn1212 <khgkjg12@naver.com>
2023-07-05 20:19:55 +0900

    프로젝트 카드 기능 추가, 디자인 변경
    - 버튼 클릭 시 새 탭에 링크 여는 기능 구현
    - 프로젝트 링크 수정
    - 디자인 수정

--- dev-blog-react/src/components/ProjectCardGrid/ProjectCard.tsx
+++ dev-blog-react/src/components/ProjectCardGrid/ProjectCard.tsx
@@ -17,6 +17,16 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
   description,
   link,
 }) => {
+  const handleClick: React.MouseEventHandler<HTMLButtonElement> = () => {
+    /*
+    noopener : 새 탭의 window.openner 참조를 null로 만든다. 원 페이지와의 브라우저 컨텍스트를 분리하여 원 페이지 컨텍스트 참조를 원천 차단함.
+    If this feature is set, the new window will not have access to the originating window via Window.opener and returns null. 
+    noreferrer : 페이지 HTTP 요청에서 referer라는 헤더를 제거. 그러면 페이지 요청을 받는 서버 입장에서는 이전 페이지에 대한 정보를 알 수가 없게된다. 
+    If this feature is set, the browser will omit the Referer header, as well as set noopener to true. See rel="noreferrer" for more information.
+    */
+    window.open(link, '_blank', 'noopener noreferrer');
+  };
+
   return (
     <Card className={styles.card}>
       <Row className="g-0">
@@ -30,7 +40,9 @@ const ProjectCard: React.FC<ProjectCardProps> = ({
           <Card.Body className={styles.previewContent}>
             <Card.Title>{title}</Card.Title>
             <Card.Text>{description}</Card.Text>
-            <Button variant="primary">View {title}</Button>
+            <Button className={styles.button} size="sm" onClick={handleClick}>
+              프로젝트 보기
+            </Button>
           </Card.Body>
         </Col>
       </Row>
