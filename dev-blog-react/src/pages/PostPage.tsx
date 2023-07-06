b5a3e57296fd3b065c5a41b67bbe3703c1917a8d
rkdgusrn1212 <khgkjg12@naver.com>
2023-07-06 11:01:43 +0900

    post page의 컴포넌트 배치 변경
    - 로우 내의 컬럼 배치를 역방향으로 해서 wrap 안됐을 떄 먼저 선언된 컬럼이 오른쪽에 오게한게 특징. 이걸로 Wrap시 리스트가 위로 가던 문제 해결

--- dev-blog-react/src/pages/PostPage.tsx
+++ dev-blog-react/src/pages/PostPage.tsx
@@ -6,12 +6,12 @@ import { Col, Row } from 'react-bootstrap';
 const PostPage: React.FC = () => {
   return (
     <ScrollViewportContainer activeHref="/#/posts">
-      <Row className="h-100">
-        <Col xs={0} md={3} className="h-100 py-2">
-          <PostList pageSize={10} pgntHalfSize={2} />
-        </Col>
-        <Col xs={12} md={9} className="h-100 overflow-scroll">
+      <Row className="h-100 flex-row-reverse">
+        <Col xs={12} xl={9} className="h-100 overflow-scroll">
           <Post />
+        </Col>
+        <Col xs={12} xl={3} className="h-100 py-2">
+          <PostList pageSize={10} pgntHalfSize={2} />
         </Col>
       </Row>
     </ScrollViewportContainer>
