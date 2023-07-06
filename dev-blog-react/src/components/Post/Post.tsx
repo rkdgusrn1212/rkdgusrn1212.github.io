824c0b9cddb7293556c4aa42d356fc93cfd3274d
rkdgusrn1212 <khgkjg12@naver.com>
2023-07-06 10:27:09 +0900

    포스트 컴포넌트 생성

---
+++ dev-blog-react/src/components/Post/Post.tsx
@@ -0,0 +1,181 @@
+import ReactMarkdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import * as dayjs from 'dayjs';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { okaidia } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+import { useParams } from 'react-router-dom';
+import { useReadPostQuery } from 'services/postApi';
+import styles from './Post.module.scss';
+
+const defaultFont = {
+  color: '#1F2318',
+  fontFamily:
+    '-apple-system, BlinkMacSystemFont,Segoe UI,Noto Sans,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji',
+};
+
+const Post: React.FC = () => {
+  const params = useParams<'idx'>();
+  const readPostResult = useReadPostQuery(parseInt(params.idx));
+
+  return (
+    <div className={'p-5 ' + styles.container}>
+      {readPostResult.isSuccess ? (
+        <>
+          <header style={{ marginBottom: '30px' }}>
+            <h1
+              style={{
+                fontSize: '42px',
+                letterSpacing: '-1px',
+                lineHeight: 1,
+              }}
+            >
+              {readPostResult.data.title}
+            </h1>
+            <p style={{ fontSize: '14px', color: '#828282' }}>
+              <time dateTime={readPostResult.data.date}>
+                {dayjs(readPostResult.data.date).format('YYYY/MM/DD HH시mm분')}
+              </time>
+            </p>
+          </header>
+          <ReactMarkdown
+            components={{
+              h1: ({ node, ...props }) => (
+                <h1
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '32px',
+                    padding: '0 0 9.6px',
+                    margin: '0 0 16px',
+                    borderBottom: '1px solid grey',
+                  }}
+                />
+              ),
+              h2: ({ node, ...props }) => (
+                <h2
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '24px',
+                    padding: '0 0 7.2px',
+                    margin: '24px 0 16px',
+                    borderBottom: '1px solid grey',
+                  }}
+                />
+              ),
+              h3: ({ node, ...props }) => (
+                <h3
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '20px',
+                    margin: '24px 0 16px',
+                  }}
+                />
+              ),
+              h4: ({ node, ...props }) => (
+                <h4
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '16px',
+                    margin: '24px 0 16px',
+                  }}
+                />
+              ),
+              h5: ({ node, ...props }) => (
+                <h5
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '14px',
+                    margin: '24px 0 16px',
+                  }}
+                />
+              ),
+              h6: ({ node, ...props }) => (
+                <h6
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '13.6px',
+                    margin: '24px 0 16px',
+                  }}
+                />
+              ),
+              p: ({ node, ...props }) => (
+                <p
+                  {...props}
+                  style={{
+                    ...defaultFont,
+                    fontSize: '16px',
+                    margin: '0 0 16px',
+                  }}
+                />
+              ),
+              pre: ({ node, ...props }) => {
+                let match = false;
+                for (const child of node.children) {
+                  if (
+                    child['tagName'] === 'code' &&
+                    /language-(\w+)/.exec(
+                      child['properties']['className'] || '',
+                    )
+                  ) {
+                    match = true;
+                    break;
+                  }
+                }
+                return match ? (
+                  <pre {...props} />
+                ) : (
+                  <pre
+                    {...props}
+                    style={{
+                      background: '#F6F8FA',
+                      margin: '0 0 16px',
+                      padding: '16px',
+                    }}
+                  />
+                );
+              },
+              code({ node, inline, className, children, ...props }) {
+                const match = /language-(\w+)/.exec(className || '');
+                return !inline && match ? (
+                  <SyntaxHighlighter
+                    {...props}
+                    children={String(children).replace(/\n$/, '')}
+                    style={okaidia}
+                    language={match[1]}
+                    PreTag="div"
+                  />
+                ) : (
+                  <code
+                    {...props}
+                    className={className}
+                    style={{
+                      ...defaultFont,
+                      fontSize: '13.6px',
+                      fontFamily:
+                        'ui-monospace,SFMono-Regular,SF Mono,Menlo,Consolas,Liberation Mono,monospace',
+                    }}
+                  >
+                    {children}
+                  </code>
+                );
+              },
+            }}
+            remarkPlugins={[remarkGfm]}
+          >
+            {readPostResult.data.body}
+          </ReactMarkdown>
+        </>
+      ) : (
+        <>로딩중</>
+      )}
+    </div>
+  );
+};
+
+export default Post;
