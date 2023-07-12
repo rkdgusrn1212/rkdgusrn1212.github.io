8a2058a1d4d8f9fe0fb94f5d73c635038793e4a8
rkdgusrn1212 <khgkjg12@naver.com>
2023-07-12 10:43:09 +0900

    footer 레이아웃 수정

--- dev-blog-react/src/components/Footer/Footer.tsx
+++ dev-blog-react/src/components/Footer/Footer.tsx
@@ -1,36 +1,42 @@
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
+import { Stack, Container } from 'react-bootstrap';
 import githubMark from './github-mark-white.svg';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
-    <Container fluid="true" className="bg-dark bg-gradient">
-      <Row className="p-1">
-        <Col>
-          <h1 className="text-light text-center">9Log</h1>
-        </Col>
-        <Col className="d-flex flex-wrap justify-content-evenly align-items-center">
-          <a className={styles.a} href="http://www.github.com/rkdgusrn1212">
+    <div className="bg-dark bg-gradient">
+      <Container fluid="md">
+        <Stack direction="horizontal" className="w-100">
+          <h1 className="text-light me-auto px-5 py-1">9Log</h1>
+          <a
+            className={'d-flex align-items-center px-2 py-2 ' + styles.a}
+            href="http://www.github.com/rkdgusrn1212"
+          >
             <img
               alt="github-logo"
               width="20px"
               height="20px"
               src={githubMark}
             />
-            &nbsp;@rkdgusrn1212
+            <span className="d-none d-md-block">&nbsp;@rkdgusrn1212</span>
           </a>
-          <a className={styles.a} href="https://solved.ac/profile/khgkjg12">
+          <a
+            className={'px-2 py-2 ' + styles.a}
+            href="https://solved.ac/profile/khgkjg12"
+          >
             <span style={{ color: 'lightgray' }}>BAE</span>
             <span style={{ color: 'skyblue' }}>{'/<'}</span>
             <span style={{ color: 'lightgray' }}>JOON</span>
-            <span style={{ color: 'skyblue' }}>{'>'}</span> khgkjg12
-            <span style={{ color: 'gold' }}>[5]</span>
+            <span style={{ color: 'skyblue' }}>{'>'}</span>
+            <span className="d-none d-md-inline">
+              {' '}
+              khgkjg12
+              <span style={{ color: 'gold' }}>[5]</span>
+            </span>
           </a>
-        </Col>
-      </Row>
-    </Container>
+        </Stack>
+      </Container>
+    </div>
   );
 }
 
