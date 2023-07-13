5a235d03eb6789032a67fbc2225b48dceb79daf1
rkdgusrn1212 <khgkjg12@naver.com>
2023-07-13 17:09:47 +0900

    footer UI수정 특히 링크를 포함한 여러 아이콘이 변경됨

--- dev-blog-react/src/components/Footer/Footer.tsx
+++ dev-blog-react/src/components/Footer/Footer.tsx
@@ -1,38 +1,31 @@
 import { Stack, Container } from 'react-bootstrap';
-import githubMark from './github-mark-white.svg';
 import styles from './Footer.module.scss';
 
 function Footer() {
   return (
-    <div className="bg-dark bg-gradient">
+    <div className={styles.footer}>
       <Container fluid="md">
         <Stack direction="horizontal" className="w-100">
-          <h1 className="text-light me-auto px-5 py-1">9Log</h1>
-          <a
-            className={'d-flex align-items-center px-2 py-2 ' + styles.a}
-            href="http://www.github.com/rkdgusrn1212"
-          >
-            <img
-              alt="github-logo"
-              width="20px"
-              height="20px"
-              src={githubMark}
-            />
-            <span className="d-none d-md-block">&nbsp;@rkdgusrn1212</span>
-          </a>
-          <a
-            className={'px-2 py-2 ' + styles.a}
-            href="https://solved.ac/profile/khgkjg12"
-          >
-            <span style={{ color: 'lightgray' }}>BAE</span>
-            <span style={{ color: 'skyblue' }}>{'/<'}</span>
-            <span style={{ color: 'lightgray' }}>JOON</span>
-            <span style={{ color: 'skyblue' }}>{'>'}</span>
-            <span className="d-none d-md-inline">
-              {' '}
-              khgkjg12
-              <span style={{ color: 'gold' }}>[5]</span>
+          <img
+            width="128px"
+            height="128px"
+            src="/react-static/logo512.png"
+            className="me-auto mx-5 my-2"
+          />
+          <a className={styles.a} href="http://www.github.com/rkdgusrn1212">
+            <img id={styles.githubIcon} />
+            <span className={'d-none d-md-block ' + styles.text} id="text">
+              @rkdgusrn1212
             </span>
+          </a>
+          <a className={styles.a} href="https://solved.ac/profile/khgkjg12">
+            <span id={styles.beakjunIcon}>
+              BAE
+              <span className={styles.character}>{'/<'}</span>
+              JOON
+              <span className={styles.character}>{'>'}</span>
+            </span>
+            <span className={'d-none d-md-block ' + styles.text}>khgkjg12</span>
           </a>
         </Stack>
       </Container>
