f3756be79debcf231530481f4a2e65f3a4242bcf
rkdgusrn1212 <khgkjg12@naver.com>
2023-08-14 00:52:01 +0900

    update : seo 기능과 연계된 hydrate 기능 제거
    - react-snap 으로 생성된 html에 hydrate로 페이지를 렌더링 할 시 UI 불일치로 인해 오류 메시지 발생, 어차피 생성된 HTML파일들은 전부 약식이므로 전부 처음부터 랜더링 하게끔 hydrate 기능을 제거

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,4 @@ import App from './App';
 import ReactDom from 'react-dom/client';
 
 const rootElement = document.getElementById('root');
-const root = ReactDom.createRoot(rootElement);
-if (rootElement.hasChildNodes()) {
-  ReactDom.hydrateRoot(rootElement, <App />);
-} else {
-  root.render(<App />);
-}
+ReactDom.createRoot(rootElement).render(<App />);
